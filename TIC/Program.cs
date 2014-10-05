07401df391fe792c9e055838159670a0e182e81b
hyuglim <hyuglim@gmail.com>
2014-10-04 20:26:49 -0400

    added client

--- TIC/Program.cs
+++ TIC/Program.cs
@@ -17,6 +17,8 @@ namespace TIC
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TICForm());
+            Client.ReceiveData();
+
         }
     }
 }
