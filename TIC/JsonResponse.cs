9d9ca5010b8b88afa907c6fcecf24bef3fb24d4f
hyuglim <hyuglim@gmail.com>
2014-10-05 03:31:22 -0400

    added Json response parser and pojo

---
+++ TIC/JsonResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization.Json.DataContractJsonSerializer;
+using System.IO;
+
+
+namespace TIC
+{
+    class JsonResponse
+    {
+        public JsonWord[,] ParseJson(String json) {
+            MemoryStream stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json));
+            DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(JsonWord[,]));
+            JsonWord[,] response = (JsonWord[,])jsonSerializer.ReadObject(stream);
+            return response;
+        }
+    }
+}
---
+++ TIC/JsonWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+
+namespace TIC
+{
+
+    [DataContract]
+    class JsonWord
+    {
+        [DataMember(Name = "text")]
+        public string text { get; set; }
+
+        [DataMember(Name = "x")]
+        public int x { get; set; }
+
+        [DataMember(Name = "y")]
+        public int y { get; set; }
+    }
+}
