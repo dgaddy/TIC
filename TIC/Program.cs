438f26da71259ad6e514a760e96d0b60bd9b0269
Ben Mattinson <bmatt@mit.edu>
2014-10-05 00:24:50 -0400

    Hotkeys for focus.

--- TIC/Program.cs
+++ TIC/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace TIC
 {
@@ -16,7 +18,12 @@ namespace TIC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TICForm());
+
+            NotifyIcon ni = new NotifyIcon();
+            ni.Visible = true;
+
+            TICForm form = new TICForm();
+            Application.Run();
         }
     }
 }
