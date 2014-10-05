e695dae9178f178d0d5f4cd78f476262f7e00817
Raymond Wu <raywu22@mit.edu>
2014-10-05 03:30:57 -0400

    Added OCRServer.

---
+++ OCRServer/OCRServer/OCRServer.Windows/Server.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+using Windows.Storage.Streams;
+using Windows.Data.Json;
+
+using WindowsPreview.Media.Ocr;
+
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace OCRServer
+{
+    class Server
+    {
+        private StreamSocketListener serverListener;
+        public static String port = "3011";
+        
+
+        private OcrEngine ocrEngine;
+
+        //Initialize the server
+        public Server()
+        {
+            ocrEngine = new OcrEngine(OcrLanguage.English);
+
+            serverListener = new StreamSocketListener();
+            
+            listen();
+        }
+
+        //Create the listener which is waiting for connection
+        private async void listen()
+        {
+            serverListener.ConnectionReceived += OnConnection;
+            try
+            {
+
+                //await serverListener.BindEndpointAsync(hostName, port);
+                await serverListener.BindServiceNameAsync(port);
+                //MainWindow.Current.UpdateLog("Listening for Connection(s)");
+            }
+            catch (Exception exception)
+            {
+                //MainWindow.Current.UpdateLog("Exception throw in Listen : " + exception);
+            }
+
+        }
+
+        //When a connection appears, this function his called
+        private async void OnConnection(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+
+            //MainWindow.Current.UpdateLog("A message has been received...");
+            //if (MainWindow.Current.loadingPage)
+            //{
+            //    MainWindow.Current.UpdateLog("wait please");
+            //}
+
+
+            DataReader reader = new DataReader(args.Socket.InputStream);
+
+            try
+            {
+                
+                int width = reader.ReadInt32();
+                int height = reader.ReadInt32();
+                byte[] inStream = new byte[width*height];
+                reader.ReadBytes(inStream);
+                
+                var ocrResult = await ocrEngine.RecognizeAsync((uint)width, (uint)height, inStream);
+
+                var lines = ocrResult.Lines;
+
+                JsonArray jsonLines = new JsonArray();
+                foreach(var line in lines) {
+                    JsonArray jsonWords = new JsonArray();
+                    foreach(var word in line.Words) {
+                        JsonObject wordObject = new JsonObject();
+                        String wordText = word.Text;
+                        int x = word.Left + word.Width / 2;
+                        int y = word.Top + word.Height / 2;
+                        
+                        wordObject.Add("text", JsonValue.CreateStringValue(wordText));
+                        wordObject.Add("x", JsonValue.CreateNumberValue(x));
+                        wordObject.Add("y", JsonValue.CreateNumberValue(y));
+
+                        jsonWords.Add(wordObject);
+                    }
+                    jsonLines.Add(jsonWords);
+                }
+
+                var outStream = args.Socket.OutputStream;
+
+                var dataWriter = new DataWriter(args.Socket.OutputStream);
+                dataWriter.WriteString(jsonLines.Stringify());
+
+            }
+            catch (Exception exception)
+            {
+                // If this is an unknown status it means that the error is fatal and retry will likely fail.
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+
+                //MainWindow.Current.UpdateLog("Read stream failed with error: " + exception.Message);
+            }
+            finally { 
+                
+            }
+
+        }
+
+    }
+}
---
+++ OCRServer/Server.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace OCR
+{
+    class Server
+    {
+        private StreamSocketListener serverListener;
+        public static String port = "3011";
+        public static String adressIP = "192.168.173.1";
+        private HostName hostName;
+
+        //Initialize the server
+        public Server()
+        {
+            serverListener = new StreamSocketListener();
+            hostName = new HostName(adressIP);
+            listen();
+        }
+
+        //Create the listener which is waiting for connection
+        private async void listen()
+        {
+            serverListener.ConnectionReceived += OnConnection;
+            try
+            {
+
+                //await serverListener.BindEndpointAsync(hostName, port);
+                await serverListener.BindServiceNameAsync(port);
+                //MainWindow.Current.UpdateLog("Listening for Connection(s)");
+            }
+            catch (Exception exception)
+            {
+                //MainWindow.Current.UpdateLog("Exception throw in Listen : " + exception);
+            }
+
+        }
+
+        //When a connection appears, this function his called
+        private async void OnConnection(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+
+            //MainWindow.Current.UpdateLog("A message has been received...");
+            //if (MainWindow.Current.loadingPage)
+            //{
+            //    MainWindow.Current.UpdateLog("wait please");
+            //}
+
+
+            DataReader reader = new DataReader(args.Socket.InputStream);
+
+            try
+            {
+                byte[] inStream;
+                reader.ReadBytes(out inStream);
+                ImageConverter ic = new ImageConverter();
+                Image img = (Image)ic.ConvertFrom(inStream);
+                Bitmap bit = new Bitmap(img);
+                var ocrResult = await ocrEngine.RecognizeAsync((uint)bitmap.PixelHeight, (uint)bitmap.PixelWidth, bitmap.PixelBuffer.ToArray());
+
+                var lines = ocrResult.Lines;
+
+                JsonArray jsonLines = new JsonArray();
+                foreach(var line in lines) {
+                    JsonArray jsonWords = new JsonArray();
+                    foreach(var word in line.Words) {
+                        JsonObject wordObject = new JsonObject();
+                        String wordText = word.Text;
+                        int x = word.Left + word.Width / 2;
+                        int y = word.Top + word.Height / 2;
+                        wordObject.Add("text", wordText);
+                        wordObject.Add("x", x);
+                        wordObject.Add("y", y);
+
+                        jsonWords.add(wordObject);
+                    }
+                    jsonLines.Add(jsonWords);
+                }
+
+
+                fjdkal; fjksda;
+                var outStream = args.Socket.OuputStream;
+
+                outStream.WriteAsync(jsonLines.Stringify());
+
+            }
+            catch (Exception exception)
+            {
+                // If this is an unknown status it means that the error is fatal and retry will likely fail.
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+
+                //MainWindow.Current.UpdateLog("Read stream failed with error: " + exception.Message);
+            }
+            finally { 
+                
+            }
+
+        }
+
+    }
+}
